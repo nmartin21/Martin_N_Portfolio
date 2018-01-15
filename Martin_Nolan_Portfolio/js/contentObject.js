71e5ea57ed4f927ddf4a7e8f6eb12ccdee5ad577
Nolan Martin <33910147+nmartin21@users.noreply.github.com>
2018-01-15 16:31:11 -0500

    Add files via upload

---
+++ Martin_Nolan_Portfolio/js/contentObject.js
@@ -0,0 +1,97 @@
+var dynamicContent = {
+	
+		image1: {
+			text : "This is a concept I made with inspiration from Stranger Things. Made in Cinema4D/Octane was a good choice because of the fog.",
+
+			headline : "Stay Awake",
+
+			
+		},
+	
+	image2: {
+			text : "This is a concept I made with inspiration from Stranger Things. Made in Cinema4D/Octane was a good choice because of the fog.",
+
+			headline : "Where It Sleeps",
+
+			
+		},
+	
+	image3: {
+			text : "This is a concept I made with inspiration from Stranger Things. Made in Cinema4D/Octane was a good choice because of the fog.",
+
+			headline : "Stay Awake pt2",
+
+			
+		},
+	
+	image4: {
+			text : "This was something I made one night with some lack of sleep. Using Quixel Megascans for the trees.",
+
+			headline : "Alone Time",
+
+			
+		},
+	
+	image5: {
+			text : "This was a scene inspired by Rainbow6 Siege, using Video Copilots Metropolitan for buildings.",
+
+			headline : "Motive",
+
+			
+		},
+	
+	image6: {
+			text : "This was the start of doing portaits for me, I wanted more practice with lighting and camera settings.",
+
+			headline : "Subject01",
+
+			
+		},
+	
+	image7: {
+			text : "Another portrait study, using displacements.",
+
+			headline : "Subject02",
+
+			
+		},
+	
+	image8: {
+			text : "This was my first commissioned peice, using more of a retrowave color pallete.",
+
+			headline : "Daze",
+
+			
+		},
+	
+	image9: {
+			text : "Another Scene inpsired by Rainbow6 Siege, first time using Adobe Fuse",
+
+			headline : "Breach",
+
+			
+		},
+	
+	image10: {
+			text : "Another portrait practice, using a snow shader for texture.",
+
+			headline : "Subject03",
+
+			
+		},
+	
+	image11: {
+			text : "This is one of the first seriouse animations I have completed. Using some models from turbosquid due to it being an animation practice.",
+
+			headline : "Animation01",
+
+			
+		},
+	
+	image12: {
+			text : "This is a concept I made in high school, to this day I think this was the picture that made me want to persue this as a job.",
+
+			headline : "Imagine",
+
+			
+		},

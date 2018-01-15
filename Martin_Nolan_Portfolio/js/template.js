ef23ddd472be5658dfe8f25492878807986932dc
Nolan Martin <33910147+nmartin21@users.noreply.github.com>
2018-01-15 18:36:47 -0500

    Add files via upload

--- Martin_Nolan_Portfolio/js/template.js
+++ Martin_Nolan_Portfolio/js/template.js
@@ -1,9 +1,41 @@
 // JavaScript Document
 
 //Variables
-
+ var theImages = document.querySelectorAll('.image-holder'),
 
 //Functions
+(function () {
+  var theImages = document.querySelectorAll('.image-holder'),
+      
+	  
+	  bckgrndLightboxColor = document.querySelector('.lightbox-content-container'),
+	  
+      appliedClass;
+
+      function changeElements() {
+	  }
+
+	
+	
+	
+          newSubImg.classList.add('thumb');
+         
+          newSubImg.src = "images/" + objectIndex.images[index];
 
 
+         
+          newSubImg.dataset.index = index;
+
+
+
+        
+          newSubImg.addEventListener('click', function() { popLightbox(index, objectIndex); }, false);
+	
+	
+	  bckgrndLightboxColor.classList.add(this.id); 
+        theSubhead.classList.add(this.id);
+        theHeader.classList.add(this.id);
+		  
+	
+	
 //Listeners
